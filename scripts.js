e7fdd83a56d635d5f99db345057fdc8fe26d360d
Randy Foo <randyfoo07@gmail.com>
2019-12-30 15:18:34 +1100

    Prepare icons for later use

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//html
+//<i class="fas fa-hand-rock"></i> 
+//<i class="fas fa-hand-scissors"></i>
+//<i class="fas fa-hand-paper"></i>
+//<i class="fas fa-fire"></i>
+//<i class="fas fa-tint"></i>
+//<i class="fas fa-leaf"></i>
